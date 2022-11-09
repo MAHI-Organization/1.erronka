04725454458c49e85606c686010c67463896e5c4
Hodei Olivas <olivas.hodei@uni.eus>
2022-11-09 14:08:05 +0100

    Azkenengo bertsioa
    Guztia bateratuta dago

---
+++ InterfazeGarapena/AzkenengoBertsioa/Erronka1/Erronka1/Models/product_template.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erronka1.Models
+{
+    public partial class product_template
+    {
+      [Key]
+
+      public int id { get; set; }
+
+     public string name { get; set; }
+
+     public int categ_id { get; set; }
+
+     public double list_price { get; set; }
+
+
+     public virtual List<sale_order_line> Sale_Order_Lines { get; set; }
+    }
+}
---
+++ InterfazeGarapena/BertsioZaharrak2/erronka1DLL/erronka1DLL/erronka_grafikoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace erronka1DLL
+{
+    public partial class erronka_grafikoa: UserControl
+    {
+        public erronka_grafikoa()
+        {
+            InitializeComponent();
+        }
+    }
+}
